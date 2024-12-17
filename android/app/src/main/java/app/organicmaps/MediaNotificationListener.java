2e6aaf0621440ad88af6f3c5dab2b2cc308a8fa5
Erik D. <62072464+erikd256@users.noreply.github.com>
2024-12-17 18:41:44 +0100

    added NotificationListener to be allowed to check for currently playing media

---
+++ android/app/src/main/java/app/organicmaps/MediaNotificationListener.java
@@ -0,0 +1,10 @@
+package app.organicmaps;
+
+import android.media.session.MediaController;
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
+public class MediaNotificationListener extends MediaController.Callback {
+}
---
+++ android/app/src/main/java/app/organicmaps/NotificationListener.java
@@ -0,0 +1,15 @@
+package app.organicmaps;
+
+import android.content.Context;
+import android.service.notification.NotificationListenerService;
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationListener extends NotificationListenerService {
+    // Helper method to check if our notification listener is enabled. In order to get active media
+    // sessions, we need an enabled notification listener component.
+    public static boolean isEnabled(Context context) {
+        return NotificationManagerCompat
+                .getEnabledListenerPackages(context)
+                .contains(context.getPackageName());
+    }
+}
